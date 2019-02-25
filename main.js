417a3438a4251357e9d2ed75030adfd75440e3d3
vjhawkins <vjhawkins@yahoo.com>
2019-02-24 18:10:49 -0800

    Lessons 3: Conditional Rendering - Use v-if, v-else-if, v-if, which updates the element in the DOM - Use v-show which updates the display property

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        description: 'The pair of socks to wear today.',
+        product: 'Socks',
+        altText: 'A pair of socks',
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        inStock: true,
+        inventory: 35,
+    }
+})
