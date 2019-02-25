36b0071391e1f15b1f70617df41476602db8bd91
vjhawkins <vjhawkins@yahoo.com>
2019-02-24 18:50:04 -0800

    Loop over array of data - Use v-for to loop over array - Use v-bind:key so that Vue can keep track of each node

--- main.js
+++ main.js
@@ -7,5 +7,43 @@ var app = new Vue({
         image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
         inStock: true,
         inventory: 35,
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        sizes: [
+            {
+                id: 0,
+                name: 'x-small'
+             },
+             {
+                 id: 1,
+                 name: 'small',
+             },
+             {
+                 id: 2,
+                 name: 'meduim',
+             },
+             {
+                 id: 3,
+                 name: 'large',
+             },
+             {
+                 id: 4,
+                 name: 'x-large',
+             },
+             {
+                 id: 5,
+                 name: 'xx-large'
+             }
+        ],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ]
     }
 })
